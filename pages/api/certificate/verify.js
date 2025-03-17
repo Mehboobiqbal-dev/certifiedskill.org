9696080db9598385526e0762418033e411171f6d
mehboobmehboob090 <mehboobmehboob090@gmail.com>
2025-03-17 13:47:57 +0000

    updated

--- pages/api/certificate/verify.js
+++ pages/api/certificate/verify.js
@@ -1,5 +1,5 @@
 // pages/api/certificate/verify.js
-import clientPromise from '../../../lib/mongodb';
+import clientPromise from '../../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
