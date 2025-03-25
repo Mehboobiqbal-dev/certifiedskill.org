bde10e18b62f8372d8e43e962bf86152915eea3c
mehboobmehboob090 <mehboobmehboob090@gmail.com>
2025-03-25 11:59:37 +0000

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,27 @@ const nextConfig = {
         fs: false,
         path: false,
         zlib: false,
-        // If needed, add other modules such as 'crypto' or 'stream'
       };
     }
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // Apply the headers to all routes.
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "frame-ancestors https://monospace.corp.google.com https://localhost.corp.google.com:10443;",
+          },
+        ],
+      },
+    ];
   },
 };
 
